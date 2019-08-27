2f97ebc42dbb1524d9c4f618f8ea5800427452f2
Abe <abrahan.rodri@gmail.com>
2019-08-27 19:48:43 -0400

    got routers working

--- config/connection.js
+++ config/connection.js
@@ -4,25 +4,27 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
- } else {
-mysql.createConnection({
+} else {
+connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Whookid3838.",
   database: "burger"
 });
+};
 
- }
+ 
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
